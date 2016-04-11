0aa39fccf6479d629c26b2a941fdd7c896289bf2
Troy Dai <troy.dai@outlook.com>
2016-04-11 16:04:35 -0700

    Update the gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 
-gulp.task('default', ['restore']);
+gulp.task('default', ['restore', 'build']);
 
 gulp.task('build', function() {
     return gulp.src('./src/**/project.json')
