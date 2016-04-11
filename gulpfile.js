29d38cd270c4af95cc268f56600b1754f7143b03
Troy Dai <troy.dai@outlook.com>
2016-04-11 16:03:07 -0700

    add gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+
+gulp.task('default', ['restore']);
+
+gulp.task('build', function() {
+    return gulp.src('./src/**/project.json')
+        .pipe(shell(['dotnet build <%= file.path %>']));
+});
+
+gulp.task('restore', function() {
+    return gulp.src('./src/**/project.json')
+        .pipe(shell(['dotnet restore <%= file.path %>']));
+});
